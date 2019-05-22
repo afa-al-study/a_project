04542666ed5b81a1d97e6e4f8b3510382dd2c672
DESKTOP-FFLFGB0\ceoah <ceoahn95@gmail.com>
2019-05-22 13:07:49 +0900

    삼성문제_17140 / 이중배열문제 - while문 for문으로 수정

--- backjoon/src/_17140.java
+++ backjoon/src/_17140.java
@@ -24,21 +24,32 @@ public class _17140 { //이차원 배열과 연산
         }
         int cnt = 0;
 
-        while(!(state[r][c] == k) || cnt > 100){//같을 때 까지
+        while(!(state[r][c] == k) && cnt <= 100){//같을 때 까지
             //행 증가
             if(row >= col){
+                int tmp = col;
+                col = 0;
                 for(int i = 1; i <= row; i++) {//행 개수만큼 돌리면서
                     int n = 1;
                     int[] n_cnt = new int[101];
 //                    ArrayList<Integer[]> n_sort = new ArrayList<>();
                     int[][] n_sort = new int[101][101];
 
-                    while (state[i][n] != 0) {//열 끝까지 돌리기
-                        n_cnt[state[i][n]] += 1; //해당 숫자 개수 올리기
-                        state[i][n] = 0;
+//                    while (state[i][n] != 0) {//열 끝까지 돌리기
+//                        n_cnt[state[i][n]] += 1; //해당 숫자 개수 올리기
+//                        state[i][n] = 0;
+//
+//                        n++;
+//                    }
 
-                        n++;
+                    for(int j = 1; j <= tmp; j++){
+                        if(state[i][j] != 0){
+                            n_cnt[state[i][j]] += 1;
+                            state[i][j] = 0;
+                        }
                     }
+
+
 
                     ArrayList<Integer> counts = new ArrayList<>();
                     for(int l = 1; l <= 99; l++){
@@ -53,10 +64,6 @@ public class _17140 { //이차원 배열과 연산
 
                     Collections.sort(counts);
 
-                    if(row < counts.size()*2) {
-                        row = counts.size() * 2;
-                    }
-
                     int q = 1;
                      while(!counts.isEmpty()){
                         for(int j = 1; j <= 99; j++){
@@ -68,9 +75,102 @@ public class _17140 { //이차원 배열과 연산
                             }
                         }
                         counts.remove(0);
+                         if(col < (q-1)) {
+                             col = (q-1);
+                         }
+
+                     }
+
+                }
+            }
+            else{ //열 증가
+                int tmp = row;
+                row = 0;
+                System.out.println("늘어날 때 col " + col);
+                for(int i = 1; i <= col; i++) {//행 개수만큼 돌리면서
+                    int n = 1;
+                    int[] n_cnt = new int[101];
+                    int[][] n_sort = new int[101][101]; //행(숫자)에 따른 열(횟수)
+
+                    for(int j = 1; j <= tmp; j++){
+                        if(state[j][i] != 0){
+                            n_cnt[state[j][i]] += 1;
+                            state[j][i] = 0;
+                        }
+                    }
+//                    while (state[n][i] != 0) {//열 끝까지 돌리기
+//                        n_cnt[state[n][i]] += 1; //해당 숫자 개수 올리기
+//                        state[n][i] = 0;
+//                        n++;
+//                    }
+
+                    ArrayList<Integer> counts = new ArrayList<>();
+                    for(int l = 1; l <= 99; l++){
+                        if(n_cnt[l] > 0){ //숫자 갯수가 1 이상일 때
+                            int count = n_cnt[l];
+                            n_sort[l][count] = 1; // 'l'숫자가 count만큼 있다
+                            if(!counts.contains(count)) {
+                                counts.add(count);
+                            }
+                        }
                     }
 
-                    //l대신 queue같은걸 이용해서 99개까지안도는 방법 찾아보기
+                    Collections.sort(counts);
+
+
+                    int q = 1;
+                    while(!counts.isEmpty()){
+                        for(int j = 1; j <= 99; j++){
+                            int g = counts.get(0); // 횟수
+                            if(n_sort[j][g] == 1 && q <= 100){ //j는 숫자 g은 횟수
+                                state[q][i] = j;
+                                state[q+1][i] = g;
+                                q = q+2;
+                            }
+                        }
+                        if(row < (q-1)) {
+                            row = (q-1);
+                        }
+                        counts.remove(0);
+                    }
+
+                }
+
+            }
+
+            System.out.println("");
+            System.out.println(cnt + " 번째!!" + " row = " + row + ", col = " + col);
+            m_func.printMatrix(row+1, col+1, state);
+
+
+
+            cnt++; //한번 증가
+        }
+//
+//        m_func.printMatrix(100, 100, state);
+        if(cnt > 100){
+            System.out.println(-1);
+
+        }else {
+            System.out.println(cnt);
+        }
+
+
+    }
+
+    //이차행렬 프린트 함수
+    public void printMatrix(int x_num, int y_num, int[][] state){
+        for(int i = 0; i < x_num; i++){
+            for(int j = 0; j < y_num; j++){
+                System.out.print(state[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
+
+
+//l대신 queue같은걸 이용해서 99개까지안도는 방법 찾아보기
 
 //                    for(int l = 1; l <= 99; l++){
 //                        if(n_cnt[l] > 0){ // 숫자 개수가 1개 이상일 때
@@ -106,38 +206,3 @@ public class _17140 { //이차원 배열과 연산
 //                            q = q+2;
 //                        }
 //                    }
-
-                }
-            }
-            else{ //열 증가
-
-
-            }
-
-
-
-
-            cnt++; //한번 증가
-        }
-
-        m_func.printMatrix(100, 100, state);
-        if(cnt > 100){
-            System.out.println(-1);
-
-        }else {
-            System.out.println(cnt);
-        }
-
-
-    }
-
-    //이차행렬 프린트 함수
-    public void printMatrix(int x_num, int y_num, int[][] state){
-        for(int i = 0; i < x_num; i++){
-            for(int j = 0; j < y_num; j++){
-                System.out.print(state[i][j] + " ");
-            }
-            System.out.println("");
-        }
-    }
-}
