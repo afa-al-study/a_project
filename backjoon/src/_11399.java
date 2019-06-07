40a7592e5a460c724f9520c7ed772184f68496a8
DESKTOP-FFLFGB0\ceoah <ceoahn95@gmail.com>
2019-06-07 13:43:14 +0900

    그리디알고리즘_ATM_11399 / 직접 sort 구현

--- backjoon/src/_11399.java
+++ backjoon/src/_11399.java
@@ -11,9 +11,11 @@ public class _11399 { //그리디 알고리즘_ATM
         for(int i = 0; i < n; i++){
             int tmp = sc.nextInt();
             p_times.add(tmp);
+
         }
 
         Collections.sort(p_times);
+
 
         int sum = 0;
         int tmp = 0;
@@ -26,3 +28,53 @@ public class _11399 { //그리디 알고리즘_ATM
         System.out.println(sum);
     }
 }
+
+
+//import java.util.ArrayList;
+//        import java.util.Collections;
+//        import java.util.Scanner;
+//
+//public class _11399 { //그리디 알고리즘_ATM
+//
+//    public static void main(String args[]){
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt(); //사람 수
+//        ArrayList<Integer> p_times = new ArrayList<>();
+//        for(int i = 0; i < n; i++){
+//            int tmp = sc.nextInt();
+//            if(i == 0){
+//                p_times.add(tmp);
+//            }
+//            else {
+//                for (int j = 0; j < p_times.size(); j++) {
+//                    if (p_times.get(j) >= tmp) {
+//                        if (j == 0)
+//                            p_times.add(0, tmp);
+//                        else
+//                            p_times.add(j, tmp);
+//
+//                        break;
+//                    } else if (j == p_times.size() - 1) {
+//                        p_times.add(tmp);
+//                        break;
+//                    }
+//                }
+//            }
+////                p_times.add(tmp);
+//
+//        }
+//
+////        Collections.sort(p_times);
+//
+//
+//        int sum = 0;
+//        int tmp = 0;
+//        for(int k : p_times){
+////            System.out.println("sum = " + sum + " / " + "k = " + k);
+//            tmp = tmp + k;
+//            sum += tmp;
+//        }
+//
+//        System.out.println(sum);
+//    }
+//}
