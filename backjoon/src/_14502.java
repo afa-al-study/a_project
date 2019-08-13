00b50240d3e3f5001f2e41182d2c9a7811f75122
DESKTOP-FFLFGB0\ceoah <ceoahn95@gmail.com>
2019-08-13 14:19:13 +0900

    no message

---
+++ backjoon/src/_14502.java
@@ -0,0 +1,7 @@
+public class _14502 { //bfs_연구소 //완전탐색 //백트랙킹
+
+    public static void main(String args[]){
+
+    }
+
+}
---
+++ backjoon/src/_1931.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class _1931 {
+
+    public static void main(String args[]){
+
+    }
+
+}
